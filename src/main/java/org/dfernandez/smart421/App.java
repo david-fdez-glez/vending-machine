20407f7c30e2d271369bc8d9a49ef3e0274264da
David Fernández González <fernandezgonzalez.d@gmail.com>
2016-04-30 18:09:17 +0100

    First Commit

---
+++ src/main/java/org/dfernandez/smart421/App.java
@@ -0,0 +1,13 @@
+package org.dfernandez.smart421;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
