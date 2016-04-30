a39dd1e5bbb337163b2cf4a51cc60f24c851cbc8
David Fernández González <fernandezgonzalez.d@gmail.com>
2016-04-30 20:42:33 +0100

    Part 1 Complete

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# vending-machine
+Welcome to Vending Machine
+
+These instructions assume that you already have installed Java 8
+(JDK 1.8).
+
+---- RUN IN AN IDE ----
+
+* Open menu: File-> Import-> Maven-> Existing Maven Projects...
+
+
+---- COMPILE AND RUN ON THE COMMAND LINE ----
+
+cd <directory>
+
+mvn clean install
+
+java -jar target/vending-machine-1.0-SNAPSHOT.jar
+
+
+            If you know how to compile programs on the command line, and if you have
+            downloaded the examples, you can easily compile and run all the examples.
+            Just change into one of the chapter directories inside "sources" and use
+            the command
+                              javac *.java
+
+            to compile all the examples from that chapter.  As long as your compiler supports
+            Java 7 or higher, there should be no errors.  (You might see some warnings, especially
+            if you use a newer version of java, but warnings do not stop a program from being
+            compiled or executed.)  You can then run individual programs using the java command.
+            For example:
+
+                              java HelloWorld
--- src/main/java/org/dfernandez/smart421/App.java
+++ src/main/java/org/dfernandez/smart421/App.java
@@ -1,5 +1,14 @@
 package org.dfernandez.smart421;
 
+import org.dfernandez.smart421.model.Coin;
+import org.dfernandez.smart421.service.VendorMachineService;
+import org.dfernandez.smart421.service.VendorMachineServiceImpl;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +17,29 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        VendorMachineService vendorMachineService = new VendorMachineServiceImpl();
+        Collection<Coin> coinsResultList;
+
+        System.out.println( "Welcome to our Vending Machine" );
+        System.out.println( "Type number of pence to see the optimal change. (Any character will exit)" );
+
+        Scanner in = new Scanner(System.in);
+
+        int entry;
+
+
+        while(in.hasNextInt()) {
+
+            entry = in.nextInt();
+
+
+            coinsResultList = vendorMachineService.getOptimalChangeFor(entry);
+            System.out.println( "Optimal Change for " +entry + " pence is " +coinsResultList);
+            System.out.println("\n");
+
+            System.out.println( "Type number of pence to see the optimal change. (Any character will exit)" );
+        }
+
     }
 }
